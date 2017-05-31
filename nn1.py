88990aac15ed2adcbcdd984ea159d87567d2c4ff
kevindevincentis <devinkmd@gmail.com>
2017-05-31 13:34:32 -0400

    First commit of first NN code

---
+++ nn1.py
@@ -0,0 +1,29 @@
+import scipy.io as sio
+from neuron import h, gui
+
+vals = sio.loadmat('../MNIST/training_values.mat')
+images = vals['images']
+
+h('''load_file("network.hoc")
+objref nn
+nn = new fullLayer(28*28)''')
+
+h('numInputs = 1')
+h.numInputs = len(images[0])
+h('double img[numInputs]')
+
+for i in range(len(images[0])):
+    h.img[i] = images[0][i]
+
+h('nn.input(img)')
+
+h.tstop = 30
+print "About to RUN"
+
+h.run()
+
+try:
+    input('Exit by pressing a key')
+except: SyntaxError
+
+h.run()
