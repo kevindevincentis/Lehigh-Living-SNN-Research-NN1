f3102a740750eff242b8d51dbf4e19fc16152b4d
kevindevincentis <devinkmd@gmail.com>
2017-06-05 16:27:27 -0400

    Added back-prop code. Found spike times in nn1.py. Ready to train?

--- back-prob.py
+++ back-prob.py
@@ -1,4 +1,8 @@
 # Code adpated from https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+def calc_weight_changes(outputs, truths, inputs):
+    LEARNING_RATE = 0.5
+    nWeights = len(inputs[0])
+    return train(output, truths, inputs, nWeights, LEARNING_RATE)
 
 def train(outputs, truths, inputs, nWeights, LEARNING_RATE):
     # 1. Output neuron deltas
@@ -22,39 +26,17 @@ def train(outputs, truths, inputs, nWeights, LEARNING_RATE):
             # Δw = α * ∂Eⱼ/∂wᵢ
             weightDetlas -= LEARNING_RATE * pd_error_wrt_weight
 
-def calculate_total_error(self, training_sets):
+def calculate_total_error(training_sets):
     total_error = 0
     for t in range(len(training_sets)):
         training_inputs, truths = training_sets[t]
-        self.feed_forward(training_inputs)
         for o in range(len(truths)):
-            total_error += self.output_layer.neurons[o].calculate_error(truths[o])
+            total_error += calculate_error(truths[o])
     return total_error
-
-def calculate_output(self, inputs):
-    self.inputs = inputs
-    self.output = self.squash(self.calculate_total_net_input())
-    return self.output
-
-def calculate_total_net_input(self):
-    total = 0
-    for i in range(len(self.inputs)):
-        total += self.inputs[i] * self.weights[i]
-    return total + self.bias
 
 # Apply the logistic function to squash the output of the neuron
 # The result is sometimes referred to as 'net' [2] or 'net' [1]
-def squash(self, total_net_input):
-    return 1 / (1 + math.exp(-total_net_input))
 
-# Determine how much the neuron's total input has to change to move closer to the expected output
-#
-# Now that we have the partial derivative of the error with respect to the output (∂E/∂yⱼ) and
-# the derivative of the output with respect to the total net input (dyⱼ/dzⱼ) we can calculate
-# the partial derivative of the error with respect to the total net input.
-# This value is also known as the delta (δ) [1]
-# δ = ∂E/∂zⱼ = ∂E/∂yⱼ * dyⱼ/dzⱼ
-#
 def calculate_pd_error_wrt_total_net_input(target_output, output):
     return calculate_pd_error_wrt_output(output, target_output) * calculate_pd_total_net_input_wrt_input(output);
 
@@ -62,35 +44,8 @@ def calculate_pd_error_wrt_total_net_input(target_output, output):
 def calculate_error(output, target_output):
     return 0.5 * (target_output - output) ** 2
 
-# The partial derivate of the error with respect to actual output then is calculated by:
-# = 2 * 0.5 * (target output - actual output) ^ (2 - 1) * -1
-# = -(target output - actual output)
-#
-# The Wikipedia article on backpropagation [1] simplifies to the following, but most other learning material does not [2]
-# = actual output - target output
-#
-# Alternative, you can use (target - output), but then need to add it during backpropagation [3]
-#
-# Note that the actual output of the output neuron is often written as yⱼ and target output as tⱼ so:
-# = ∂E/∂yⱼ = -(tⱼ - yⱼ)
 def calculate_pd_error_wrt_output(output, target_output):
     return -(target_output - output)
 
-# The total net input into the neuron is squashed using logistic function to calculate the neuron's output:
-# yⱼ = φ = 1 / (1 + e^(-zⱼ))
-# Note that where ⱼ represents the output of the neurons in whatever layer we're looking at and ᵢ represents the layer below it
-#
-# The derivative (not partial derivative since there is only one variable) of the output then is:
-# dyⱼ/dzⱼ = yⱼ * (1 - yⱼ)
 def calculate_pd_total_net_input_wrt_input(output):
     return output * (1 - output)
-
-# The total net input is the weighted sum of all the inputs to the neuron and their respective weights:
-# = zⱼ = netⱼ = x₁w₁ + x₂w₂ ...
-#
-# The partial derivative of the total net input with respective to a given weight (with everything else held constant) then is:
-# = ∂zⱼ/∂wᵢ = some constant + 1 * xᵢw₁^(1-0) + some constant ... = xᵢ
-def calculate_pd_total_net_input_wrt_weight(self, index):
-    return self.inputs[index]
-
-def calc_weight_changes(outputs, truth):
--- nn1.py
+++ nn1.py
@@ -2,7 +2,7 @@ import scipy.io as sio
 from neuron import h, gui
 from matplotlib import pyplot
 
-threshold = 10
+threshold = 0
 vals = sio.loadmat('../MNIST/training_values.mat')
 images = vals['images']
 
@@ -44,16 +44,30 @@ for i in range(len(axarr)):
     # ax.xlabel('time (ms)')
     # ax.ylabel('outCells[0] mV')
 
-done = False
+done = True
+foundWin = False
+t_truth = 0
+spike_times = [0] * len(outputs)
 for i in range(len(outputs[0])):
+    done = True
     for j in range(10):
         if outputs[j][i] >= threshold:
-            print "Winner is digit %d, %d" %(j, i)
-            done = True
-            break
-    if (done): break
+            if not foundWin:
+                print "Winner is digit %d at %dms" %(j, i/40.0)
+                foundWin = True
+                t_truth = i/40.0
+            if spike_times[j] == 0: spike_times[j] = i/40.0
+        if spike_times[j] == 0: done = False
+    if done: break
+
+truths = [0] * len(outputs)
+for i in range(len(outputs)):
+    if i == labels[0]: truths[i] = t_truth
+    else: truths[i] = t_truth + 5
+
 
 # pyplot.show()
+
 try:
     input('Exit by pressing a key')
 except: SyntaxError
