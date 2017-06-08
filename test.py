b116e927e414c58104f3e8ac0e9b394ed6260086
kevindevincentis <devinkmd@gmail.com>
2017-06-08 15:36:42 -0400

    Changed some testing code

--- test.py
+++ test.py
@@ -6,12 +6,14 @@ images = vals['images']
 
 h('''load_file("network.hoc")
 objref nn
-nn = new fullLayer(28*28)''')
+nn = new fullLayer(4)''')
 
-img = images[0]
+img = [0.03333, .03333, .054265, 0.03333]
 h('numInputs = 1')
 h.numInputs = len(img)
 h('double img[numInputs]')
+
+print(images[0][0], images[0][1], images[0][2], images[0][3])
 
 for i in range(len(img)):
     h.img[i] = img[i]
